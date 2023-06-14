9bdf1282924b87147f3a153af625b886de841e09
Ricky <Rhruan2@gmail.com>
2023-06-13 21:46:26 -0400

    first commit

---
+++ src/firebase/index.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-ikmkBeKBUGVH1nkdcoqJxFx88excBN4",
+  authDomain: "fir-bf8cc.firebaseapp.com",
+  projectId: "fir-bf8cc",
+  storageBucket: "fir-bf8cc.appspot.com",
+  messagingSenderId: "1066552938509",
+  appId: "1:1066552938509:web:9346523a8d2e8aeec64544",
+  measurementId: "G-YYH4JGKXFE",
+};
+
+// Initialize Firebase
+const config = initializeApp(firebaseConfig);
+export const auth = getAuth(config);
+export const firestore = getFirestore(config);
